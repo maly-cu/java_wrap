ae82f5542880df9a1fbf8ec5a16801108b7b2b0a
Malynda <phoebe95.cu@gmail.com>
2023-12-03 17:53:54 +0100

    Simple notification with java wrapped around python using pyjnius: cast and autoclass

--- main.py
+++ main.py
@@ -106,24 +106,6 @@ Int = autoclass("java.lang.Integer")
 AlarmManager = autoclass('android.app.AlarmManager')
 
 
-intent = Intent()
-intent.setClass(context, Reminder_Service)
-# Here change the "org.org.test" to whatever package domain you have set.
-# Here my buildozer file has the package domain of "org.test".
-# After that "NOTIFY" is the custom action we have set. This custom action is
-# also defined in the manifest file(check README file). You can use any name here
-# just make sure you use the same name while registering the action event
-intent.setAction("org.org.test.REMIND")
-# Create a pending intent to be fired later in time by the alarm Manager
-# Here the intent_id is a variable holding a numeric value that uniquely identifies the
-# pending intent. Keep this id so that you can cancel scheduled alarms later on.
-
-# There are various types of pending intent flags that can be set based on what you want.
-# Here the `FLAG_CANCEL_CURRENT` will cancel any other pending intent with the same id before
-# setting itself.
-pending_intent = PendingIntent.getBroadcast(
-    context, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT
-)
 
 class MyApp(App):
     # The only thing this class will need is a build function
@@ -138,9 +120,27 @@ class MyApp(App):
         button.bind(on_press=self.create_notification)
         return button
 
-    def create_notification(self, instance):
+    def create_notification(self, instance):  # instance is for the button press done thru python, if kv file, no need
+        intent = Intent()
+        intent.setClass(context, Reminder_Service)
+        # Here change the "org.org.test" to whatever package domain you have set.
+        # Here my buildozer file has the package domain of "org.test".
+        # After that "NOTIFY" is the custom action we have set. This custom action is
+        # also defined in the manifest file(check README file). You can use any name here
+        # just make sure you use the same name while registering the action event
+        intent.setAction("org.org.test.REMIND")
+        # Create a pending intent to be fired later in time by the alarm Manager
+        # Here the intent_id is a variable holding a numeric value that uniquely identifies the
+        # pending intent. Keep this id so that you can cancel scheduled alarms later on.
+
+        # There are various types of pending intent flags that can be set based on what you want.
+        # Here the `FLAG_CANCEL_CURRENT` will cancel any other pending intent with the same id before
+        # setting itself.
+        pending_intent = PendingIntent.getBroadcast(
+            context, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT
+        )
         # This gets the current system time since epoch in milliseconds(works only in python 3.7+)
-        ring_time = time.time_ns() // 1_000_000
+        ring_time = time.time_ns() // 1_000_000 * 2
         # We now create the alarm and assign it to the system alarm manager. Some methods assign
         # an alarm manager instance to a variable and then scheduling a task. But if you need to
         # later cancel this alarm from another python file or from another launch of your app(as
